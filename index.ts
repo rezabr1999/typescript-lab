0ec051c4c8504b5cc5c1c61ceadce553b4aa65b3
Reza Br <reza.linusx@gmail.com>
2024-08-21 18:49:52 +0330

    setup typescript project

---
+++ index.ts
@@ -0,0 +1,7 @@
+// first use npm init --yes to create a package.json file
+// then use npm install typescript --save-dev to install typescript
+// then use tsc --init to create a tsconfig.json file
+// then use tsc to compile the code
+// use tsc --watch to watch for changes
+
+console.log("Hello, World!");
